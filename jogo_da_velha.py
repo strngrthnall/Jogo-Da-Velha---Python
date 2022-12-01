3ebe222839ba80e5050ccbd9ae0bbabf11c3c2e9
Strngrthnall <marcos.miranda.ext@outlook.com>
2022-12-01 16:58:52 -0300

    initial commit

---
+++ jogo_da_velha.py
@@ -0,0 +1,301 @@
+from time import sleep
+import os
+
+clear = lambda: os.system('cls')
+clear()
+
+def vitoria(jogador):
+    if(jogador == 2):
+        sleep(0.25)
+        clear()
+        print(" ██████╗     ██╗   ██╗███████╗███╗   ██╗ ██████╗███████╗██████╗  ██████╗ ██████╗    ")
+        print("██╔═══██╗    ██║   ██║██╔════╝████╗  ██║██╔════╝██╔════╝██╔══██╗██╔═══██╗██╔══██╗██╗")
+        print("██║   ██║    ██║   ██║█████╗  ██╔██╗ ██║██║     █████╗  ██║  ██║██║   ██║██████╔╝╚═╝")
+        print("██║   ██║    ╚██╗ ██╔╝██╔══╝  ██║╚██╗██║██║     ██╔══╝  ██║  ██║██║   ██║██╔══██╗██╗")
+        print("╚██████╔╝     ╚████╔╝ ███████╗██║ ╚████║╚██████╗███████╗██████╔╝╚██████╔╝██║  ██║╚═╝")
+        print(" ╚═════╝       ╚═══╝  ╚══════╝╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝   ")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        print("                                                                                    ")
+        print("     ██╗ ██████╗  ██████╗  █████╗ ██████╗  ██████╗ ██████╗     ██████╗              ")
+        print("     ██║██╔═══██╗██╔════╝ ██╔══██╗██╔══██╗██╔═══██╗██╔══██╗    ╚════██╗             ")
+        print("     ██║██║   ██║██║  ███╗███████║██║  ██║██║   ██║██████╔╝     █████╔╝             ")
+        print("██   ██║██║   ██║██║   ██║██╔══██║██║  ██║██║   ██║██╔══██╗    ██╔═══╝              ")
+        print("╚█████╔╝╚██████╔╝╚██████╔╝██║  ██║██████╔╝╚██████╔╝██║  ██║    ███████╗             ")
+        print(" ╚════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝    ╚══════╝             ")
+    elif(jogador == 1):
+        sleep(0.25)
+        clear()
+        print(" ██████╗     ██╗   ██╗███████╗███╗   ██╗ ██████╗███████╗██████╗  ██████╗ ██████╗    ")
+        print("██╔═══██╗    ██║   ██║██╔════╝████╗  ██║██╔════╝██╔════╝██╔══██╗██╔═══██╗██╔══██╗██╗")
+        print("██║   ██║    ██║   ██║█████╗  ██╔██╗ ██║██║     █████╗  ██║  ██║██║   ██║██████╔╝╚═╝")
+        print("██║   ██║    ╚██╗ ██╔╝██╔══╝  ██║╚██╗██║██║     ██╔══╝  ██║  ██║██║   ██║██╔══██╗██╗")
+        print("╚██████╔╝     ╚████╔╝ ███████╗██║ ╚████║╚██████╗███████╗██████╔╝╚██████╔╝██║  ██║╚═╝")
+        print(" ╚═════╝       ╚═══╝  ╚══════╝╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝   ")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        sleep(0.125)
+        print("-")
+        print("                                                                                    ")
+        print("     ██╗ ██████╗  ██████╗  █████╗ ██████╗  ██████╗ ██████╗      ██╗                 ")
+        print("     ██║██╔═══██╗██╔════╝ ██╔══██╗██╔══██╗██╔═══██╗██╔══██╗    ███║                 ")
+        print("     ██║██║   ██║██║  ███╗███████║██║  ██║██║   ██║██████╔╝    ╚██║                 ")
+        print("██   ██║██║   ██║██║   ██║██╔══██║██║  ██║██║   ██║██╔══██╗     ██║                 ")
+        print("╚█████╔╝╚██████╔╝╚██████╔╝██║  ██║██████╔╝╚██████╔╝██║  ██║     ██║                 ")
+        print(" ╚════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝     ╚═╝                 ")
+    else:
+        sleep(0.25)
+        clear()
+        print("██████╗ ███████╗██╗   ██╗    ██╗   ██╗███████╗██╗     ██╗  ██╗ █████╗ ██╗")
+        print("██╔══██╗██╔════╝██║   ██║    ██║   ██║██╔════╝██║     ██║  ██║██╔══██╗██║")
+        print("██║  ██║█████╗  ██║   ██║    ██║   ██║█████╗  ██║     ███████║███████║██║")
+        print("██║  ██║██╔══╝  ██║   ██║    ╚██╗ ██╔╝██╔══╝  ██║     ██╔══██║██╔══██║╚═╝")
+        print("██████╔╝███████╗╚██████╔╝     ╚████╔╝ ███████╗███████╗██║  ██║██║  ██║██╗")
+        print("╚═════╝ ╚══════╝ ╚═════╝       ╚═══╝  ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝")
+
+tabuleiro = [[" "," "," "],[" "," "," "],[" "," "," "]]
+jogador = 1
+jogada = 0
+fim = False
+
+print("#######################################################################")
+print("##############################-Bem Vindo-##############################")
+print("#######################################################################")
+print("")
+sleep(2)
+clear()
+sleep(0.5)
+print("       █████                                      ")
+sleep(0.125)
+print("      ░░███                                       ")
+sleep(0.125)
+print("       ░███   ██████   ███████  ██████            ")
+sleep(0.125)
+print("       ░███  ███░░███ ███░░███ ███░░███           ")
+sleep(0.125)
+print("       ░███ ░███ ░███░███ ░███░███ ░███           ")
+sleep(0.125)
+print(" ███   ░███ ░███ ░███░███ ░███░███ ░███           ")
+sleep(0.125)
+print("░░████████  ░░██████ ░░███████░░██████            ")
+sleep(0.125)
+print(" ░░░░░░░░    ░░░░░░   ░░░░░███ ░░░░░░             ")
+sleep(0.125)
+print("                      ███ ░███                    ")
+sleep(0.125)
+print("                     ░░██████                     ")
+sleep(0.125)
+print("                      ░░░░░░                      ")
+sleep(0.125)
+print("           █████                                  ")
+sleep(0.125)
+print("          ░░███                                   ")
+sleep(0.125)
+print("        ███████   ██████                          ")
+sleep(0.125)
+print("       ███░░███  ░░░░░███                         ")
+sleep(0.125)
+print("      ░███ ░███   ███████                         ")
+sleep(0.125)
+print("      ░███ ░███  ███░░███                         ")
+sleep(0.125)
+print("      ░░████████░░████████                        ")
+sleep(0.125)
+print("       ░░░░░░░░  ░░░░░░░░                         ")
+sleep(0.125)
+print("                                                  ")
+sleep(0.125)
+print("                                                  ")
+sleep(0.125)
+print("                                                  ")
+sleep(0.125)
+print(" █████   █████          ████  █████               ")
+sleep(0.125)
+print("░░███   ░░███          ░░███ ░░███                ")
+sleep(0.125)
+print(" ░███    ░███   ██████  ░███  ░███████    ██████  ")
+sleep(0.125)
+print(" ░███    ░███  ███░░███ ░███  ░███░░███  ░░░░░███ ")
+sleep(0.125)
+print(" ░░███   ███  ░███████  ░███  ░███ ░███   ███████ ")
+sleep(0.125)
+print("  ░░░█████░   ░███░░░   ░███  ░███ ░███  ███░░███ ")
+sleep(0.125)
+print("    ░░███     ░░██████  █████ ████ █████░░████████")
+sleep(0.125)
+print("     ░░░       ░░░░░░  ░░░░░ ░░░░ ░░░░░  ░░░░░░░░ ")
+sleep(0.125)
+print("                                                  ")
+sleep(0.125)
+print("")
+print("#######################################################################")
+print("#######################################################################")
+print("\n\n")
+sleep(2)
+clear()
+
+print("Tutorial: Para jogar cada jogador deve escolher uma casa para marcar,\nusando numeros de 0 à 2 para escolher a linha e depois a coluna,\nassim escolhendo uma casa.")
+print("#######################################################################")
+print("Primeiro escolha o numero da linha, depois o numero da coluna, como mostra o exemplo abaixo:")
+print("[(0,0)] [(0,1)] [(0,2)]")
+print("[(1,0)] [(1,1)] [(1,2)]")
+print("[(2,0)] [(2,1)] [(2,2)]")
+print("#######################################################################")
+
+while(True):
+    print("Começar o jogo?")
+    iniciar = input("(S/N): ")
+    iniciar = iniciar.capitalize()
+    
+    if(iniciar == "S"):
+        break
+    elif(iniciar == "N"):
+        exit()
+
+sleep(2)
+clear()
+
+print("  0  1  2")
+for i in range(3):
+    print(i, end='')
+    for j in range(3):
+        
+        print("[" + tabuleiro[i][j] + "]", end='')
+    print("")
+
+while(fim == False):
+    if(jogada < 9):
+        if(jogador == 1):
+            print("\nVocê é o jogador " + str(jogador) + ", escolha seu movimento.")
+            linha = int(input("Digite a linha desejada: "))
+            coluna = int(input("Digite a coluna desejada: "))
+            
+            if((linha < 3)and(coluna < 3)and(tabuleiro[linha][coluna] == ' ')):
+                tabuleiro[linha][coluna] = 'X'
+                print("  0  1  2")
+                for i in range(3):
+                    print(i, end='')
+                    for j in range(3):
+                        print("[" + tabuleiro[i][j] + "]", end='')
+                    print("")
+            else:
+                print("Posição inválida, tente novamente")
+            
+            for k in range(3):
+                if((tabuleiro[k][0] == 'X') and (tabuleiro[k][1] == 'X') and (tabuleiro[k][2] == 'X')):
+                    vitoria(jogador)
+                    fim = True
+                    break
+                elif((tabuleiro[0][k] == 'X') and (tabuleiro[1][k] == 'X') and (tabuleiro[2][k] == 'X')):
+                    vitoria(jogador)
+                    fim = True
+                    break
+                elif((tabuleiro[0][0] == 'X') and (tabuleiro[1][1]== 'X') and (tabuleiro[2][2] == 'X')):
+                    vitoria(jogador)
+                    fim = True
+                    break
+                elif((tabuleiro[0][2] == 'X') and (tabuleiro[1][1] == 'X') and (tabuleiro[2][0] == 'X')):
+                    vitoria(jogador)
+                    fim = True
+                    break
+            jogador+=1
+            jogada+=1
+
+            
+        elif(jogador == 2):
+            print("\nVocê é o jogador " + str(jogador) + ", escolha seu movimento.")
+            linha = int(input("Digite a linha desejada: "))
+            coluna = int(input("Digite a coluna desejada: "))
+            
+            if((linha < 3)and(coluna < 3)and(tabuleiro[linha][coluna] == ' ')):
+                tabuleiro[linha][coluna] = 'O'
+                print("  0  1  2")
+                for i in range(3):
+                    print(i, end='')
+                    for j in range(3):
+                        print("[" + tabuleiro[i][j] + "]", end='')
+                    print("")
+            else:
+                print("Posição inválida, tente novamente")
+            
+            for k in range(3):
+                if((tabuleiro[k][0] == 'O') and (tabuleiro[k][1] == 'O') and (tabuleiro[k][2] == 'O')):
+                    vitoria(jogador)
+                    fim = True
+                    break
+                elif((tabuleiro[0][k] == 'O') and (tabuleiro[1][k] == 'O') and (tabuleiro[2][k] == 'O')):
+                    vitoria(jogador)
+                    fim = True
+                    break
+                elif((tabuleiro[0][0] == 'O') and (tabuleiro[1][1]== 'O') and (tabuleiro[2][2] == 'O')):
+                    vitoria(jogador)
+                    fim = True
+                    break
+                elif((tabuleiro[0][2] == 'O') and (tabuleiro[1][1] == 'O') and (tabuleiro[2][0] == 'O')):
+                    vitoria(jogador)
+                    fim = True
+                    break
+            jogador-=1
+            jogada+=1
+
+    else:
+        vitoria(0)
+        break
